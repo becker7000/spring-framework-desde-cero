6f769146ef46f85883828c5f0a1220a7778d1d31
Erick Alvarez <42850106+becker7000@users.noreply.github.com>
2025-01-12 08:39:32 -0600

    Delete market/market directory

--- market/market/src/main/java/mx/com/cst/webapp/market/domain/repository/ProductoRepository.java
+++
@@ -1,8 +0,0 @@
-package mx.com.cst.webapp.market.domain.repository;
-
-import mx.com.cst.webapp.market.persistence.entity.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto,Integer> {
-
-}
